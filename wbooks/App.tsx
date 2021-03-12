16e51b7ca1a926c1d6017a697f79d1e707ed4daf
hernan-clich <hernan.clich@wolox.com.ar>
2021-03-12 15:38:51 -0300

    Added Reactotron config

--- wbooks/App.tsx
+++ wbooks/App.tsx
@@ -1,3 +1,9 @@
+if (__DEV__) {
+  import('./src/config/reactotronConfig').then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Reactotron Configured');
+  });
+}
 import React from 'react';
 
 import App from './src/app';
