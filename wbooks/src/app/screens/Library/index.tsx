dd601a9fd8436f2ac104bd8a309f52592eaccf5f
hernan-clich <hernan.clich@wolox.com.ar>
2021-03-16 19:35:03 -0300

    Updates in Library component: keyExtractor and removed View component (added those styles to the FlatList directly)

--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, ListRenderItem, View } from 'react-native';
+import { FlatList, ListRenderItem } from 'react-native';
 import SingleBook from '@components/SingleBook';
 import { BOOKS_MOCK } from '@constants/mockBooks';
 import { Book } from '@interfaces/books';
@@ -11,12 +11,15 @@ function Library() {
   const renderBooks: ListRenderItem<Book> = ({ item }) => (
     <SingleBook bookCover={item.imageUrl} bookTitle={trimLineBreaks(item.title)} bookAuthor={item.author} />
   );
-  const keyExtractor = (item: Book) => item.id.toString();
+  const keyExtractor = ({ id }: Book) => String(id);
 
   return (
-    <View style={styles.container}>
-      <FlatList data={BOOKS_MOCK} renderItem={renderBooks} keyExtractor={keyExtractor} />
-    </View>
+    <FlatList
+      style={styles.container}
+      data={BOOKS_MOCK}
+      renderItem={renderBooks}
+      keyExtractor={keyExtractor}
+    />
   );
 }
 
